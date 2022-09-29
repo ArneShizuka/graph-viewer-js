a125ffb1816ea0acc2b33fc95a3e388bcca83e76
AngeloIacona <iaconaangelo.ai@gmail.com>
2022-09-29 20:00:09 +0200

    refactor: create buildGraph and modify addNodes and addEdges

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,48 +1,66 @@
-const cy = cytoscape({
-    container: document.getElementById('cy'),
+const buildGraph = () => {
+    const cy = cytoscape({
+        container: document.getElementById("cy"),
+        layout: { name: "preset" },
+        style: [
+            {
+                selector: "node",
+                style: {
+                    label: "data(id)",
+                },
+            },
+            {
+                selector: "edge",
+                style: {
+                    label: "data(timestamp)",
+                    width: 3,
+                    "line-color": "#ccc",
+                    "target-arrow-color": "#ccc",
+                    "target-arrow-shape": "triangle",
+                    "curve-style": "bezier",
+                },
+            },
+        ],
+    })
 
-    layout: { name: 'preset' },
+    return cy
+}
 
-    style: [
-        {
-            selector: 'node',
-            style: {
-                'label': 'data(id)'
-            }
-        },
-        {
-            selector: 'edge',
-            style: {
-                'label': 'data(timestamp)',
-                'width': 3,
-                'line-color': '#ccc',
-                'target-arrow-color': '#ccc',
-                'target-arrow-shape': 'triangle',
-                'curve-style': 'bezier'
-            }
-        }
-    ]
-
-})
-
-const addNodes = (numNodes) => {
+const addNodes = (graph, numNodes) => {
     for (let i = 0; i < numNodes; i++) {
-        cy.add({ group: 'nodes', data: { id: `n${i}` }, position: { x: Math.floor(i / 2) * 100, y: (i % 3) * 100 } })
+        graph.add({
+            group: "nodes",
+            data: { id: `n${i}` },
+            position: { x: Math.floor(i / 2) * 100, y: (i % 3) * 100 },
+        })
     }
 }
 
-const addEdges = (edgeProb) => {
-    cy.nodes().forEach(e1 => {
-        cy.nodes().forEach(e2 => {
+const addEdges = (graph, edgeProb) => {
+    graph.nodes().forEach((e1) => {
+        graph.nodes().forEach((e2) => {
             if (Math.random() < edgeProb) {
-                cy.add({ group: 'edges', data: { id: `e${cy.edges().length}`, source: e1.data('id'), target: e2.data('id'), timestamp: Math.floor(Math.random() * 30) } })
+                graph.add({
+                    group: "edges",
+                    data: {
+                        id: `e${graph.edges().length}`,
+                        source: e1.data("id"),
+                        target: e2.data("id"),
+                        timestamp: Math.floor(Math.random() * 30),
+                    },
+                })
             }
         })
     })
 }
 
-addNodes(10)
-addEdges(0.25)
+const randomGraph = (numNodes = 10, edgeProb = 0.25) => {
+    const graph = buildGraph()
+    addNodes(graph, numNodes)
+    addEdges(graph, edgeProb)
+    return graph
+}
 
+const cy = randomGraph()
 cy.center()
-cy.fit(padding = 100)
+cy.fit((padding = 100))
